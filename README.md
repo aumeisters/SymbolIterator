4286e29b8fdd247473eb2ffa5e2462f6833d02a4
aumeisters <aumeister@inbox.lv>
2020-03-29 12:55:10 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+HW Iterable Objects
---
+++ index.js
@@ -0,0 +1,29 @@
+class Iterable {
+  constructor(num) {
+    this.num = num;
+  }
+  [Symbol.iterator]() {
+    let num = this.num;
+    let startPoint = 0;
+    return {
+      next() {
+        let result = { value: undefined, done: true };
+        if (startPoint < num) {
+          result.value = String(Math.round(Math.random()))
+            .concat(".")
+            .concat(
+              String(Math.floor(Math.random() * 10)).concat(
+                String(Math.floor(Math.random() * 10))
+              )
+            );
+          result.done = false;
+          startPoint += 1;
+        }
+        return result;
+      }
+    };
+  }
+}
+
+const it = new Iterable(3);
+console.log([...it]);
